73147dc7f2a5c9bb88b6266cda44ec02350b0518
coralineGG <coraline.guigou6@gmail.com>
2022-12-02 14:03:36 +0100

    script of the 6th question
    kruskal because i'm not sure the anova is applyable in this case

---
+++ 6_analsysis_merci.R
@@ -0,0 +1,41 @@
+# working directory
+getwd()
+setwd("D:/études/SUPAGRO/2A/D4/github/vineyards")
+
+# library 
+library(tidyverse)
+library(agricolae)
+
+# get the data
+merci_cc_ini = read.table("6_merci.csv", header = TRUE, sep = ";", dec = ",")
+view(merci_cc_ini)
+
+
+merci_cc <- merci_cc_ini%>%
+  select (Line, Z1, Z2, Z3)%>%
+  pivot_longer(cols = Z1:Z3, names_to = "zone",values_to = "merci_value" )
+view(merci_cc)
+#Graph 
+library(ggplot2)
+merci_cc%>%
+  group_by(Line)%>%
+  summarise(mymean=mean(merci_value, na.rm=T),
+            mysd=sd(merci_value, na.rm=T))%>%
+  mutate(ymax=mymean+mysd, ymin=mymean-mysd)%>%
+  ggplot (aes(x = reorder(Line, mymean), y = mymean, ymin=ymin, ymax=ymax)) + 
+  geom_point()+
+  geom_errorbar()+
+  labs(x="Line", y="Cover rate", title = "Percentage of coverance of the soil for different cover crops")
+
+#Anova line
+anova.merci1.l<-lm(merci_value~Line, merci_cc)
+anova(anova.merci1.l)
+par(mfrow = c(1,1))
+plot(anova.merci1.l)
+
+kruskal <- kruskal(merci_cc$merci_value, canopeo_weed$line, console = T)
+table.letters.merci <- kruskal$groups %>%
+  rownames_to_column("line") %>%
+  select(line, groups)
+view(table.letters.merci)
+
